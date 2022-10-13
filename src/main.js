f51392e672f9b08a2175b50bb1de820b153cc48e
Keith Chan <keithchanyk28.17@ichat.sp.edu.sg>
2022-10-13 16:45:32 +0800

    Install bootstrap and font-awesome libraries

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
+
