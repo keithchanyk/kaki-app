acc3dba0446c8feb0ec4cbfff76171ea67089135
Keith Chan <keithchanyk28.17@ichat.sp.edu.sg>
2022-11-11 22:37:16 +0800

    styled time

--- src/main.js
+++ src/main.js
@@ -32,7 +32,22 @@ export default defineComponent({
 
     return { center }
   },
+  methods: {
+
+  }
 })
+
+
+export function tConvert(time) {
+  const timeString = time
+  // Prepend any date. Use your birthday.
+  const timeString12hr = new Date('1970-01-01T' + timeString + 'Z')
+    .toLocaleTimeString('en-US',
+      { timeZone: 'UTC', hour12: true, hour: 'numeric', minute: 'numeric' }
+    );
+
+  return timeString12hr
+}
 
 const pinia = createPinia()
 
