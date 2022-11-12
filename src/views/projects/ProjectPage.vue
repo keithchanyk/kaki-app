f8d6b1ad5aa7a41f5b0408796d9c960e946af17e
Kathy Tong <100739687+kt-x@users.noreply.github.com>
2022-11-13 01:12:17 +0800

    minor axios path adjust

--- src/views/projects/ProjectPage.vue
+++ src/views/projects/ProjectPage.vue
@@ -149,7 +149,7 @@ export default {
   methods: {
     get_details() {
       axios
-        .get('http://localhost/is216/kakidb-2/kakidb-2/project/read.php')
+        .get('http://localhostkakidb-2/project/read.php')
         .then((response) => {
           this.project_details = response.data.records;
           console.log(this.project_details);
