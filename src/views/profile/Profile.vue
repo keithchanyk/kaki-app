1df2b49a690b2240446c2782a64d06f39cb9cadc
Xavier Low <95678174+xavierlowjunjie@users.noreply.github.com>
2022-11-12 19:49:41 +0800

    profile completed activities

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -34,13 +34,30 @@ export default {
   },
   computed: {
     filteredList() {
-      console.log(this.project_details);
-      return this.project_details.filter((project) => {
-        return (
-          project.proj_name.toLowerCase().includes(this.search.toLowerCase()) ||
-          project.org_name.toLowerCase().includes(this.search.toLowerCase())
-        );
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, '0');
+      var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      var yyyy = today.getFullYear();
+
+      today = yyyy + '/' + mm + '/' + dd;
+      var result = this.project_details.filter((project) => {
+        return project.proj_date > today;
       });
+
+      return result;
+    },
+    pastEvents() {
+      var today = new Date();
+      var dd = String(today.getDate()).padStart(2, '0');
+      var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      var yyyy = today.getFullYear();
+
+      today = yyyy + '/' + mm + '/' + dd;
+      var result = this.project_details.filter((project) => {
+        return project.proj_date < today;
+      });
+
+      return result;
     },
   },
 
@@ -112,7 +129,7 @@ export default {
             </button>
             <button
               class="nav-link"
-              id="nav-profile-tab"
+              id="nav-pending-tab"
               data-bs-toggle="tab"
               data-bs-target="#nav-profile"
               type="button"
@@ -124,12 +141,12 @@ export default {
             </button>
             <button
               class="nav-link"
-              id="nav-contact-tab"
+              id="nav-complete-tab"
               data-bs-toggle="tab"
-              data-bs-target="#nav-contact"
+              data-bs-target="#nav-complete"
               type="button"
               role="tab"
-              aria-controls="nav-contact"
+              aria-controls="nav-complete"
               aria-selected="false"
             >
               Completed Activities
@@ -149,6 +166,8 @@ export default {
         </button> -->
           </div>
         </nav>
+
+        <!-- main tab content -->
         <div
           class="tab-content"
           id="nav-tabContent"
@@ -201,7 +220,9 @@ export default {
                             d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
                           />
                         </svg>
-                        <h6 class="fw-normal">&nbsp;&nbsp;{{ project.proj_date }}</h6>
+                        <h6 class="fw-normal">
+                          &nbsp;&nbsp;{{ project.proj_date }}
+                        </h6>
                         <br />
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -242,7 +263,132 @@ export default {
                             d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
                           />
                         </svg>
-                        <h6 class="fw-normal">&nbsp;&nbsp;{{ project.suitability }}</h6>
+                        <h6 class="fw-normal">
+                          &nbsp;&nbsp;{{ project.suitability }}
+                        </h6>
+                        <br />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-geo-alt-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
+                          />
+                        </svg>
+                        <h6 class="fw-normal">
+                          &nbsp;Located in: {{ project.region }}
+                        </h6>
+                        <br />
+                        <!-- <h6 style="font-size: 12px" class="fw-light">
+                          &nbsp;{{ project.total_capacity }}
+                        </h6> -->
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <!-- PROJECT DETAILS -->
+          </div>
+
+          <div
+            class="tab-pane fade"
+            id="nav-complete"
+            role="tabpanel"
+            aria-labelledby="nav-complete-tab"
+            tabindex="0"
+          >
+            <div class="container-fluid pb-4">
+              <div class="row mx-auto container-fluid">
+                <div
+                  v-if="isDateOver"
+                  v-for="project in pastEvents"
+                  :key="project.id"
+                  class="mt-4 col d-flex justify-content-start"
+                >
+                  <a
+                    class="nav-link"
+                    :href="'/projectdetails?id=' + project.id"
+                  >
+                    <div class="card projCard glass">
+                      <div
+                        class="card-header projCard-header projCard-image card-image"
+                      >
+                        <img
+                          id="card-img"
+                          class="mb-2 rounded"
+                          src="src/assets/pic1.jpg"
+                        />
+                      </div>
+                      <div class="card-body projCard-body mb-1">
+                        <h5 class="h3">{{ project.proj_name }}</h5>
+                        <h6 class="mt-5 fw-normal opacity-50">
+                          by {{ project.org_name }}
+                        </h6>
+                        <br />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-calendar"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
+                          />
+                        </svg>
+                        <h6 class="fw-normal">
+                          &nbsp;&nbsp;{{ project.proj_date }}
+                        </h6>
+                        <br />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-alarm"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z"
+                          />
+                          <path
+                            d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"
+                          />
+                        </svg>
+                        <h6 class="fw-normal">
+                          &nbsp;{{ Main.tConvert(project.starttime) }} -
+                          {{ Main.tConvert(project.endtime) }}
+                        </h6>
+                        <br />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-people-fill"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+                          />
+                          <path
+                            fill-rule="evenodd"
+                            d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216z"
+                          />
+                          <path
+                            d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"
+                          />
+                        </svg>
+                        <h6 class="fw-normal">
+                          &nbsp;&nbsp;{{ project.suitability }}
+                        </h6>
                         <br />
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -277,24 +423,6 @@ export default {
             id="nav-profile"
             role="tabpanel"
             aria-labelledby="nav-profile-tab"
-            tabindex="0"
-          >
-            ...
-          </div>
-          <div
-            class="tab-pane fade"
-            id="nav-contact"
-            role="tabpanel"
-            aria-labelledby="nav-contact-tab"
-            tabindex="0"
-          >
-            ...
-          </div>
-          <div
-            class="tab-pane fade"
-            id="nav-disabled"
-            role="tabpanel"
-            aria-labelledby="nav-disabled-tab"
             tabindex="0"
           >
             ...
